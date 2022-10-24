51f0d3e2992d80ff0cd7899067dec6515bc06c3c
mutchmel <110733752+mutchmel@users.noreply.github.com>
2022-10-23 23:00:27 -0500

    Add files via upload

---
+++ python-ds-practice/01_product/product.py
@@ -0,0 +1,15 @@
+def product(a, b):
+    a = 2
+    b = 2
+    return (a * b)
+    return (a * (-1 * b))
+
+
+    """Return product of a and b.
+
+        >>> product(2, 2)
+        4
+
+        >>> product(2, -2)
+        -4
+    """
---
+++ python-ds-practice/03_last_element/last_element.py
@@ -0,0 +1,10 @@
+def last_element(lst):
+    last_element = ([1, 2, 3])
+    """Return last item in list (None if list is empty.
+    
+        >>> last_element([1, 2, 3])
+        3
+        
+        >>> last_element([]) is None
+        True
+    """
